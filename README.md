2c79a2dc0e2a51596266d4407ff649737ae6ef18
deniseAngilica <deangilica@gmail.com>
2020-05-04 09:40:20 +0200

    Introduce wrapping folder

--- README.md
+++
@@ -1,2 +0,0 @@
-# EmbASP4Unity
-
--- it/unical/mat/embasp/specializations/dlv/DLVFilterOption.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EmbASP4Unity.it.unical.mat.embasp.specializations.dlv
-{
-	using ASPFilterOption = it.unical.mat.embasp.languages.asp.ASPFilterOption;
-
-	public class DLVFilterOption : ASPFilterOption
-	{
-    public DLVFilterOption(string initial_option) : base() => options += initial_option;
-  }
-}
