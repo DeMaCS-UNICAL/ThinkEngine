e1efe402db70ec6d7cfb2021ad94fa6a9b366770
deniseAngilica <deangilica@gmail.com>
2020-06-08 10:10:26 +0200

    reintroduced .meta files

---
+++ UnityAsset/ThinkEngine/it/unical/mat/testCecil.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using Mono.Cecil;
+
+
+
+public class testCecil
+{
+    // Start is called before the first frame update
+
+
+    //[MenuItem("Assets/Run Cecil")]
+    //[PostProcessScene(0)]
+    public static void Run()
+    {
+        using (var assembly = AssemblyDefinition.ReadAssembly("./Library/ScriptAssemblies", new ReaderParameters { ReadWrite = true }))
+        {
+            // ... Injection code goes here
+
+            Debug.Log(assembly.FullName);
+        }
+    }
+}
